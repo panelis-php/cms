98b4a2c049447c90596a454ae68df78c783d30a0
Yugo <dedy.purwanto@levart.id>
2026-05-12 14:38:44 +0700

    Register event/listener for setting

--- modules/Setting/SettingServiceProvider.php
+++ modules/Setting/SettingServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Modules\Setting;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Modules\Setting\Events\SettingUpdated;
+use Modules\Setting\Listeners\FlushCache;
 
 class SettingServiceProvider extends ServiceProvider
 {
@@ -13,6 +16,8 @@ class SettingServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
 
         $this->loadViewsFrom(__DIR__.'/Views', 'setting');
+
+        Event::listen(SettingUpdated::class, FlushCache::class);
     }
 
     public function register(): void
